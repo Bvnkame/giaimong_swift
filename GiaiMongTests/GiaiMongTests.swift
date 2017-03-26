8848808c6987aec6e02e666626dbfab15cce2918
NGUYEN QUOC THANH <nguyenquocthanh@quincy.local>
2017-03-26 09:36:49 +0700

    Initial Commit

---
+++ GiaiMongTests/GiaiMongTests.swift
@@ -0,0 +1,36 @@
+//
+//  GiaiMongTests.swift
+//  GiaiMongTests
+//
+//  Created by NGUYEN QUOC THANH on 3/26/17.
+//  Copyright © 2017 NGUYEN QUOC THANH. All rights reserved.
+//
+
+import XCTest
+@testable import GiaiMong
+
+class GiaiMongTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
